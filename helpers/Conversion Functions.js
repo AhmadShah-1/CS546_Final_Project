4712c14d5dfec91402e9b1a9122995d7ca6db4d1
lukebowyer22 <lukebowyer22@gmail.com>
2024-12-15 17:48:48 -0500

    added conversion functions

---
+++ helpers/Conversion Functions.js
@@ -0,0 +1,4 @@
+export const lbsToKg = (lbs) => (lbs ? lbs * 0.453592 : lbs);
+export const kgToLbs = (kg) => (kg ? kg / 0.453592 : kg);
+export const cmToInches = (cm) => (cm ? cm / 2.54 : cm);
+export const inchesToCm = (inches) => (inches ? inches * 2.54 : inches);
