b4f1c4742e30fa0ab882090d4cdc662b115b2c3b
Syed Ahmad Shah <117304625+AhmadShah-1@users.noreply.github.com>
2024-12-16 00:45:42 -0500

    Updated login handlebars look

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 import configRoutes from './routes/index.js';
 import session from 'express-session';
+import open from 'open';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(
         saveUninitialized: true,
     })
 );
+
+app.use(express.static('public/css'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,9 +30,9 @@ app.set('views', './views');
 
 configRoutes(app);
 
-const PORT = 3002
+const PORT = 3000
 
 app.listen(PORT, async () => {
-    console.log("Server is running at http://localhost:3002");
+    console.log(`Server is running at http://localhost:${PORT}`);
     await open(`http://localhost:${PORT}`);
 });
